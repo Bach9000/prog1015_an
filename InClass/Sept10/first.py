25fb95732342f728c4eb365bba42a42c2a217307
Bach9000 <atbn8r@gmail.com>
2018-09-10 10:11:32 -0300

    Finished first in class exercise

--- InClass/Sept10/first.py
+++ InClass/Sept10/first.py
@@ -4,8 +4,15 @@
 
 def main():
     # Input and Variables
+    numberOne = input("Enter number one!")
+    numberTwo = input("Enter number two!")
+    
     # Processing
+    Total = int(numberOne) + int(numberTwo)
+
     # Output	
-	
+    print(Total)
+
+
 if __name__ == "__main__":
     main()
