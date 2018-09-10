212d643ba76ca217fae529555afed23f9f95c90e
Bach9000 <atbn8r@gmail.com>
2018-09-10 09:33:43 -0300

    Code Snippet from September10 2018

---
+++ InClass/Sept10/first.py
@@ -0,0 +1,11 @@
+# Adrian Neumann
+# 2018 Sept10 9:21 am
+# This program is my first in-class exercise
+
+def main():
+    # Input and Variables
+    # Processing
+    # Output	
+	
+if __name__ == "__main__":
+    main()
