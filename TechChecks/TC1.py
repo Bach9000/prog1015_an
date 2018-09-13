e3b20536dd200b86b314fa5308644cb8b225df73
Bach9000 <atbn8r@gmail.com>
2018-09-13 09:34:07 -0300

    PROG 1015 -- Tech Check #1: Variables, Operators, I/O & Casting

---
+++ TechChecks/TC1.py
@@ -0,0 +1,27 @@
+# Author: Adrian T. Neumann
+# Date: Sept 13, 2018
+# Description:  Simple application to calculate final restaurant bill.  
+
+
+def main():
+ 
+ 
+    # Input and Variables
+    bill = float(input("Please enter your original bill amount: "))
+    
+
+    # Processing
+    tax = bill * 0.15
+    tip = bill * 0.20
+    total = bill + tax + tip
+    
+    
+    # Output
+    print("Your original bill amount is: " + str(bill))
+    print("Your tax is: " + str(tax))
+    print("Your tip is: " + str(tip))
+    print("Your total is: " + str(total))	
+    
+    
+if __name__ == "__main__":
+    main()
