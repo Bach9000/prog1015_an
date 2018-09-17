5ffec3f74d8290063b71bea00842939c6f52b468
Bach9000 <atbn8r@gmail.com>
2018-09-17 09:56:32 -0300

    Sept 17th, Assignment #1

---
+++ Assignments/Assingment #1/Program 2–WeeklyLoanCalculator.py
@@ -0,0 +1,29 @@
+# Author: Adrian T. Neumann
+# Date: 2018, Sept 17
+# Description: Develop a short term loan calculator program as 
+# a console application with the following specifications. Begin 
+# by designing your solution to this problem in pseudocode, which 
+# will be submitted along with the program.
+# If A dollars are borrowed at r% interest compounded 
+# weekly to purchase something with weekly payments for n years, 
+# then the monthly payment is given by the formula:
+# If: i  = r/5200 
+# Then calculate the monthly payment as:
+# monthly payment  =  i/1 – (1 + i)-52n	* A
+# Write a console application that calculates the weekly payment 
+# after the user gives the amount of the loan, interest rate, and number of years.
+
+
+
+def main():
+ 
+ 
+    # Input and Variables
+    
+    # Processing
+    
+    # Output	
+    
+    
+if __name__ == "__main__":
+    main()
