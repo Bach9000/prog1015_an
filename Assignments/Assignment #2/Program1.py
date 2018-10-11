9d01883cde3bc92b77cd640c9133648ebcc5fce1
W0202213 <W0202213@AD.NET172.CA>
2018-10-11 11:17:40 -0300

    Program One Assignment # 2

---
+++ Assignments/Assignment #2/Program1.py
@@ -0,0 +1,17 @@
+# Author: Adrian T. Neumann
+# Date: 11 October 2018
+# Description: First Program from Assignmnet #2
+
+
+def main():
+ 
+ 
+    # Input and Variables
+    
+    # Processing
+    
+    # Output	
+    
+    
+if __name__ == "__main__":
+    main()
